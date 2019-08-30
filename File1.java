5786e36232dbdf854f71a8869834e7be00fda10f
poojagaana <pooja.gaana15@gmail.com>
2019-08-30 15:12:15 +0530

    changes done

--- File1.java
+++ File1.java
@@ -1,7 +1,8 @@
 
 class Simple{
 public static void main(String args[]) throws Interrupted Exception{
-System.out.println("Hello World");
 Thread.sleep(1000);
+System.out.println("Hello World");
+
 }
 }
